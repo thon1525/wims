a691e81de6786f054ae623784b75768487f41439
Thon <137491012+thon1525@users.noreply.github.com>
2025-04-13 22:07:08 +0700

    feat: deploy

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE="wims.settings"
 pip3 install -r requirements.txt  # Optional if @vercel/python handles it
+gunicorn wims.wsgi:application
 python3 manage.py collectstatic --noinput
--- index.py
+++
@@ -1,3 +0,0 @@
-from wims.asgi import application  # Make sure you have asgi.py configured in your Django project
-
-# This file lets Vercel serve Django via ASGI
